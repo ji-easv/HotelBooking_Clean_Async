340340933a362dc3acd1bf92159eee6594cebc92
Julia Ilasova <1julka1il@gmail.com>
2025-11-02 17:05:22 +0100

    clean up BookingStepDefinitions.cs

--- HotelBooking.BDDTests/StepDefinitions/BookingStepDefinitions.cs
+++ HotelBooking.BDDTests/StepDefinitions/BookingStepDefinitions.cs
@@ -16,17 +16,15 @@ namespace HotelBooking.BDDTests.StepDefinitions;
 public sealed class BookingStepDefinitions
 {
     private readonly BookingsController controller;
-    private readonly Mock<IRepository<Booking>> mockBookingRepository;
     private readonly Mock<IBookingManager> mockBookingManager = new();
 
     private IActionResult lastResult;
     private Booking bookingRequest;
-    private int bookingId;
 
     public BookingStepDefinitions()
     {
-        mockBookingRepository = Utilities.SetUpBookingRepositoryMocks();
-        controller = new BookingsController(mockBookingRepository.Object, mockBookingManager.Object);
+        var mockBookingRepository1 = Utilities.SetUpBookingRepositoryMocks();
+        controller = new BookingsController(mockBookingRepository1.Object, mockBookingManager.Object);
     }
 
     [Given("there is at least one available room")]
@@ -84,69 +82,10 @@ public sealed class BookingStepDefinitions
         Assert.Equal(conflict.Value, "The booking could not be created. All rooms are occupied. Please try another period.");
     }
 
-    [Given("a customer provides incomplete booking information")]
-    public void GivenACustomerProvidesIncompleteBookingInformation()
-    {
-        bookingRequest = null;
-    }
-
     [When("the customer submits the booking request")]
     public async Task WhenTheCustomerSubmitsTheBookingRequest()
     {
         lastResult = await controller.Post(bookingRequest);
-    }
-
-    [Then("the customer should receive a validation error message")]
-    public void ThenTheCustomerShouldReceiveAValidationErrorMessage()
-    {
-        Assert.IsType<BadRequestResult>(lastResult);
-    }
-
-    [Given("a booking exists with ID {int}")]
-    public void GivenABookingExistsWithID(int id)
-    {
-        bookingId = id;
-        mockBookingRepository.Setup(m => m.GetAsync(id)).ReturnsAsync(new Booking
-        {
-            Id = id, CustomerId = 1, RoomId = 1, StartDate = DateTime.Now, EndDate = DateTime.Now.AddDays(1),
-            IsActive = true
-        });
-    }
-
-    [When("the customer requests the booking details for ID {int}")]
-    public async Task WhenTheCustomerRequestsTheBookingDetailsForID(int id)
-    {
-        lastResult = await controller.Get(id);
-    }
-
-    [Then("the booking details should be returned")]
-    public void ThenTheBookingDetailsShouldBeReturned()
-    {
-        Assert.IsType<ObjectResult>(lastResult);
-        var obj = (lastResult as ObjectResult)?.Value as Booking;
-        Assert.NotNull(obj);
-        Assert.Equal(bookingId, obj.Id);
-    }
-
-    [When("the customer cancels the booking with ID {int}")]
-    public async Task WhenTheCustomerCancelsTheBookingWithID(int id)
-    {
-        mockBookingRepository.Setup(m => m.RemoveAsync(id)).Returns(Task.CompletedTask);
-        lastResult = await controller.Delete(id);
-    }
-
-    [Then("the booking should be removed from the system")]
-    public void ThenTheBookingShouldBeRemovedFromTheSystem()
-    {
-        Assert.IsType<Microsoft.AspNetCore.Mvc.NoContentResult>(lastResult);
-        mockBookingRepository.Verify(m => m.RemoveAsync(bookingId), Moq.Times.Once);
-    }
-
-    [Then("the room should become available for those dates")]
-    public void ThenTheRoomShouldBecomeAvailableForThoseDates()
-    {
-        // This would be handled by business logic; for BDD, we verify RemoveAsync was called
-        mockBookingRepository.Verify(m => m.RemoveAsync(bookingId), Moq.Times.Once);
     }
 
     [Given("all rooms are already booked for the requested dates")]
